3882c6babed488a11330a2c8190d863636d6a460
Nittu Rajkumar <nitturajkumar123@gmail.com>
2026-03-22 14:04:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     setError('');
     setResult(null);
     try {
-      const response = await fetch('http://localhost:5000/api/summarize', {
+      const response = await fetch('https://assignmentsummarizer-server.onrender.com/api/summarize', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
